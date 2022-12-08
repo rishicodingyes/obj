315799aa921ff4149d2761ea4f7feba7d954fb6a
rishicodingyes <95447185+rishicodingyes@users.noreply.github.com>
2022-12-08 17:50:44 -0500

    Add files via upload

---
+++ TvandAC.js
@@ -0,0 +1,49 @@
+img = "";
+status = ""
+objects =[];
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+}
+function preload()
+{
+img = loadImage("tvandac.jpg")
+}
+function draw()
+{
+image(img,0,0,640, 420)
+
+if(status !="")
+{
+    for (i = 0; i < objects.length; i++)
+    {
+        document.getElementById("status").innerHTML = "Status : Object Detected"
+
+        fill("blue")
+        percent= floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y)
+        noFill();
+stroke("blue")
+rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+    }
+}
+
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status=true;
+    objectDetector.detect(img, gotResult)
+}
+
+function gotResult(error, results)
+{
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    objects= results;
+}
